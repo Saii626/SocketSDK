144f15c395dcad36bf09257b7701286d8da14579
Saikat Dey <saikat626@gmail.com>
2019-10-24 09:02:22 +0530

    SessionData support for try with resources

--- src/main/java/app/saikat/SocketSDK/CommonFiles/SessionData.java
+++ src/main/java/app/saikat/SocketSDK/CommonFiles/SessionData.java
@@ -2,9 +2,14 @@ package app.saikat.SocketSDK.CommonFiles;
 
 import java.util.UUID;
 
-public final class SessionData {
+public final class SessionData implements AutoCloseable {
 
     private static ThreadLocal<UUID> sessionInfo = new ThreadLocal<>();
+
+    // Required for autoclosable feature try with resources
+    private static SessionData sessionData = new SessionData();
+
+    private SessionData() { throw new RuntimeException(); }
 
     public static UUID getCurrentSession() {
         return sessionInfo.get();
@@ -16,5 +21,15 @@ public final class SessionData {
 
     public static void removeSession() {
         sessionInfo.remove();
+    }
+
+    // Required for autoclosable feature
+    public SessionData getInstance() {
+        return sessionData;
+    }
+
+    @Override
+    public void close() throws Exception {
+        SessionData.removeSession();
     }
 }
